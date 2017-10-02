f333465041d421de483811832573ea8c09786b55
Carolyn Ryan <ryanc2@carleton.edu>
2017-10-02 13:59:08 -0500

    Minor changes

--- assignment2.py
+++ assignment2.py
@@ -3,38 +3,26 @@ import nltk
 
 # Changes to HW2_InputFile:
     # Added space between .It for 'It was a hell of a payoff'
+    # Addedd space between '.I love that he and'
+    # Separate all contractions
+    # Get rid of non sentence ending '.'
+
 
 def main():
     f = open("HW2_InputFile.txt", "r")
     line = f.readline()
     sentence_tokens = nltk.tokenize.sent_tokenize(line) # Doesn't work on 'F.B.I agent Dale' splits into two sentences
+    word_tokens = nltk.word_tokenize(line)
+
     print("Entire text:", line)
     print("NLTK Sentence Tokenizer:", sentence_tokens)
+    print("NLTK Word Tokenizer:", word_tokens,"\n")
 
-    new_line = ""
-    new_sentence_tokens = []
-    for sentence in sentence_tokens:
-        if sentence[0] in "abcdefghijklmnopqrstuvwxyz":
-            new_line = new_line[0:-6] + ". "
-            new_line += sentence + " </s> "
-            new_sentence_tokens[-1] += " "+sentence
-        else:
-            new_line += "<s> " + sentence[0:-1] + " </s> "
-            new_sentence_tokens.append(sentence)
-    print("Entire text with start stop:", new_line)
-    print("Entire text with new sentence measures:", new_sentence_tokens)
-
-    tokens = nltk.word_tokenize(new_line)
-    print("NLTK Word Tokenizer:", tokens)
-
-    words = new_line.split(" ")
-    print("Words split at ' ':", words)
 
     print("The number of sentences in the text is", len(sentence_tokens))
-    print("The number of words in the text is", len(words))
+    print("The number of words in the text is", len(word_tokens))
     f.close()
-
-
 
 if __name__ == '__main__':
     main()
+
