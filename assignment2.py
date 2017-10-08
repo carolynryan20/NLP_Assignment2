17ecdab4f1528648ae050a9f13448bad0b113e15
Carolyn Ryan <ryanc2@carleton.edu>
2017-10-08 12:39:21 -0500

    Some cleaning

--- assignment2.py
+++ assignment2.py
@@ -1,13 +1,9 @@
 import nltk
 
 # Changes to HW2_InputFile:
-    # Added space between .It for 'It was a hell of a payoff'
-    # Addedd space between '.I love that he and'
-    # Separate all contractions
-    # Get rid of non sentence ending '.'
-
     # The textfile used non-ascii apostrophes and quotations
     #   so we changed these to their ASCII counterparts
+    # ä to a (in doppelgänger)
 
 
 def main():
@@ -18,25 +14,20 @@ def main():
     word_tokens = tokenize_words(corpus)
     unique_word_dict = unique_words(word_tokens)
 
-    #print("Entire text (cleaned):", corpus)
-    #print("NLTK Sentence Tokenizer:", sentence_tokens)
-    #print("NLTK Word Tokenizer:", word_tokens,"\n")
-    #print
-    #print '#####################################################'
-    print
     print("The number of sentences in the text is", len(sentence_tokens))
     print("The number of words in the text is", word_count(word_tokens))
-    print unique_word_dict
     f.close()
 
 def clean_corpus(corpus):
     # Documentation on what we changed to the corpus
     corpus = corpus.replace(".I ", ". I ")
     corpus = corpus.replace(".It ", ". It ")
+    corpus = corpus.replace(".That ", ". That ")
     corpus = corpus.replace("Mr. ", "Mr ")
     corpus = corpus.replace("Dr. ", "Dr ")
     corpus = corpus.replace("F.B.I", "FBI")
     corpus = corpus.replace("Catherine E.", "Catherine E")
+    corpus = corpus.replace("ä", "a")
 
     corpus = corpus.replace("'re", " are")
     corpus = corpus.replace("'ve", " have")
@@ -50,7 +41,7 @@ def clean_corpus(corpus):
     corpus = corpus.replace("there's", "there is")
     corpus = corpus.replace("that's", "that is")
     corpus = corpus.replace("That's", "That is")
-    corpus = corpus.replace("He's", "He is")
+    corpus = corpus.replace("He's", "He is") # sometimes awkward tense, as should be 'he has' in some cases
     corpus = corpus.replace("he's", "he is")
     corpus = corpus.replace("Let's", "Let us")
 
